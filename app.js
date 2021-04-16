c5957cba77b50bcc4fdd10e76d631ac21e9a32fd
ChangeS9 <spltime31@gmail.com>
2021-04-16 12:20:32 +0530

    done

---
+++ app.js
@@ -0,0 +1,16 @@
+const input = document.querySelector(".user-input");
+const iconContainer = document.querySelector(".icon-container");
+
+// const onKeyUp = () => {
+//     console.log(input.value);
+
+//     if (input.value.length > 5) {
+//         iconContainer.style.display = "block";
+//         input.style.marginLeft = "1.3rem";
+//     }
+
+//     if (input.value.length < 5) {
+//         iconContainer.style.display = "none";
+//         input.style.marginLeft = "0rem";
+//     }
+// }
